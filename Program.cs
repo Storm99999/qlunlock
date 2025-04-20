c8e48b3678ffe28e01bce7bea964004aac82bf68
newguy <87811650+Storm99999@users.noreply.github.com>
2025-04-21 00:35:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Memory;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(@"
+
+                       /$$                     /$$                     /$$      
+                      | $$                    | $$                    | $$      
+              /$$$$$$ | $$ /$$   /$$ /$$$$$$$ | $$  /$$$$$$   /$$$$$$$| $$   /$$
+             /$$__  $$| $$| $$  | $$| $$__  $$| $$ /$$__  $$ /$$_____/| $$  /$$/
+            | $$  \ $$| $$| $$  | $$| $$  \ $$| $$| $$  \ $$| $$      | $$$$$$/ 
+            | $$  | $$| $$| $$  | $$| $$  | $$| $$| $$  | $$| $$      | $$_  $$ 
+            |  $$$$$$$| $$|  $$$$$$/| $$  | $$| $$|  $$$$$$/|  $$$$$$$| $$ \  $$
+             \____  $$|__/ \______/ |__/  |__/|__/ \______/  \_______/|__/  \__/
+                  | $$                                                          
+                  | $$                                                          
+                  |__/                                                          
+");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        Console.WriteLine(" [qlunlock] Code written by newguy, addresses by Tactical");
+        Mem m = new Mem();
+
+        int processId = m.GetProcIdFromName("quakelive_steam");
+        if (processId > 0)
+        {
+            m.OpenProcess(processId);
+
+            string address = "quakelive_steam.exe+1043098";
+            int value = 500;
+
+            bool success = m.WriteMemory(address, "int", value.ToString());
+
+            Console.WriteLine(success ? "   [qlunlock] Memory written successfully!" : "[qlunlock] Failed to write memory. Try again maybe?");
+        }
+        else
+        {
+            Console.WriteLine(" [qlunlock] Quake Live is not open!");
+        }
+        Console.ReadLine();
+    }
+}
